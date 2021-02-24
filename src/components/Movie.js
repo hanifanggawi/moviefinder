ddc896d4fefabcf70ea347ab758fec2db682a0f4
hanifanggawi <hanifanggawi45@gmail.com>
2021-02-25 00:04:43 +0700

    adjusted routing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,7 +3,7 @@ import alt from '../images/NotFoundAlt.png'
 const Movie = ({id, movie}) => {
     return (
         <div className="movie">
-            <a href={`movie/${id}`}>
+            <a href={`movie-finder-react/movie/${id}`}>
                 <img src={(movie.poster_path) ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : alt} alt={movie.title}/>
             </a>
             <h3 className="movie-title"> {movie.title} </h3>
